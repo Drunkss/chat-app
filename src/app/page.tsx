f15cd3ab620865ec2a12556c1e95d4758337e5bd
Drunkss <davidessien243@gmail.com>
2024-01-16 21:37:42 -0600

    socket test successful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
+"use client"
+
+import { useSocket } from "@/components/Providers/socket-provider";
 import { SocketIndicator } from "@/components/socket-indicator";
 //import { Badge } from "@/components/ui/badge";
-//import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 const page = () => {
+  const [ msg, setmsg ] = useState("ssss")
+  const [ click, setClick ] = useState(false)
+  const { socket } = useSocket()
+
+
+  async function testfn(){
+    setClick(true)
+    const message = await axios.get("/api/socket/test") 
+    return msg
+  }
+
+  useEffect(()=>{
+    if(!socket) return
+
+    socket.on("message",(val : string) => {
+      setmsg(val.msg)
+    })
+  },[click])
+
   return (
     <div className="p-[10rem] font-bold text-3xl">
       <SocketIndicator/>
+
+      <h1>
+        {msg}
+      </h1>
+
+      <Button onClick={() => {testfn()}}>Test</Button>
     </div>
   )
 }
---
+++ src/pages/api/socket/test/index.ts
@@ -0,0 +1,13 @@
+import { NextApiResponseServerInfo } from "@/types/types";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler( 
+  req: NextApiRequest,
+  res: NextApiResponseServerInfo
+){
+  res?.socket?.server?.io?.emit("message",{
+    msg: "Hello"
+  })
+
+  return res.status(200).json({ message: "nice :)" })
+}
